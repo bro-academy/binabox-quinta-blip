5d4b061316920f4987e37bd938c342e8cb74b47f
quinta-blip <nahbumquinta@gmail.com>
2025-09-23 15:13:48 +0300

    update link problems

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import './styles/style.scss';
+
+// Import main Twig pages so Vite compiles them
+import './pages/index.twig';   // my main page
+import './templates/pages/blog.twig';   
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,41 @@
 import { defineConfig } from 'vite';
-import vituum from 'vituum';
-import nunjucks from '@vituum/vite-plugin-nunjucks';
+import fs from 'fs';
+import path from 'path';
 import twig from '@vituum/vite-plugin-twig';
+import nunjucks from '@vituum/vite-plugin-nunjucks';
 import postcss from '@vituum/vite-plugin-postcss';
-
 import imageOptimizer from '@bro-academy/vite-plugin-image-optimizer';
 import svgSprite from '@bro-academy/vite-plugin-svg-sprite';
 
+// Load ONLY global data
+const commonData = JSON.parse(
+  fs.readFileSync(path.resolve('src/data/common.json'), 'utf-8')
+);
+
 export default defineConfig({
+  server: {
+    port: 5173,
+    strictPort: true
+  },
   build: {
     assetsInlineLimit: 0,
+    outDir: 'dist'
   },
   plugins: [
     svgSprite(),
-    vituum(),
-    twig(),
+    twig({
+      root: 'src', // Root folder for Twig templates
+      templatesDir: [
+        'src/pages',             // your page templates
+        'src/templates/layouts', // layouts like base.twig
+        'src/templates/partials' // header/footer partials
+      ],
+      globals: {
+        common: commonData
+      }
+    }),
     nunjucks(),
     postcss(),
-    imageOptimizer(),
-  ],
+    imageOptimizer()
+  ]
 });
