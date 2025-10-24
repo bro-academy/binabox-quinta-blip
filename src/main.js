a33d5f4de2e43d27dbbf274562d56b1f0938941e
quinta-blip <nahbumquinta@gmail.com>
2025-10-25 00:56:27 +0300

    BAFC5-203: Add compiled SCSS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // Import global SCSS so Vite compiles it
 import './styles/scss/main.scss';
 
-// Import main Twig pages so Vite knows about them
+// Import main Twig pages for Vite 
 import './pages/index.twig';
 import './templates/pages/blog.twig';
